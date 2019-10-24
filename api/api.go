fa6de384699b3c4461118d064f4a5c2f94bf693a
ldq <lideqiang@zingsq.cn>
2019-10-24 18:07:59 +0800

    完善 grpc-gateway 增加证书校验以及 token 认证

--- api/api.go
+++ api/api.go
@@ -1,23 +1,50 @@
 package main
 
-//func main() {
-//	ctx := context.Background()
-//	ctx, cancel := context.WithCancel(ctx)
-//	defer cancel()
-//	mux := runtime.NewServeMux()
-//	client := gtls.NewClientTLS()
-//	creds ,err :=  client.GetTLSCredentials()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	err = pb.RegisterSearchServiceHandlerFromEndpoint(ctx, mux, "localhost:9001", []grpc.DialOption{grpc.WithTransportCredentials(creds)}, )
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println("rpc  rest-api start")
-//	err = http.ListenAndServe(":8899", mux)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//}
+import (
+	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	pb "grpc_demo/proto"
+	"grpc_demo/util/auth"
+	"grpc_demo/util/conf"
+	"grpc_demo/util/gtls"
+	"log"
+	"net/http"
+)
+
+func init() {
+	conf.InitConfig()
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	client := gtls.NewClientTLS()
+	creds, err := client.GetCredentiasCA()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	option := []grpc.DialOption{
+		//证书校验
+		grpc.WithTransportCredentials(creds),
+		//token认证
+		grpc.WithPerRPCCredentials(&auth.Authentication{
+			conf.Config.Client.AppId,
+			conf.Config.Client.AppKey,
+		}),
+	}
+	err = pb.RegisterSearchServiceHandlerFromEndpoint(ctx, mux, "localhost:9001", option)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("rpc  rest-api start")
+	err = http.ListenAndServe(":8899", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
